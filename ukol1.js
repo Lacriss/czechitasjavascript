ef0d77dac431543382f67d1e89c07ec4428479bd
Lacriss <marie.duskova1@gmail.com>
2022-02-22 18:11:51 +0100

    2 files modified, 2 added

--- ukol1.js
+++ ukol1.js
@@ -1,1 +1,7 @@
-console.log('ahoj');
+let minutVHodine = 60;
+let hodinVeDne = 24;
+let dnuVLednu = 31;
+
+let vysledek = minutVHodine * hodinVeDne * dnuVLednu;
+
+console.log(vysledek);
---
+++ ukol2.js
@@ -0,0 +1,19 @@
+let jmeno = 'Lenka';
+let vek = 30;
+let vaha = 65;
+let vyska = 1.6;
+
+let bmi = vaha / (vyska * vyska);
+
+console.log(
+  'Jmenuji se ' +
+    jmeno +
+    ', je mi ' +
+    vek +
+    ' let, vážím ' +
+    vaha +
+    'kg, měřím ' +
+    vyska +
+    'm a moje BMI je ' +
+    bmi
+);
